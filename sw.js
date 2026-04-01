4dac075e54d9bf40e5c4c654ba22b36fd1330741
Tan-0412 <rawiwan815@gmail.com>
2026-04-01 13:51:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,35 @@
-const CACHE = 'timeclock-v1';
+const CACHE = 'timeclock-v2';
+
 const ASSETS = [
-  './',
-  './index.html',
-  './TimeRecord.png',
-  './manifest.json'
+  '/Kingyont/',
+  '/Kingyont/index.html',
+  '/Kingyont/TimeRecord.png',
+  '/Kingyont/manifest.json'
 ];
 
+// install
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      )
     )
   );
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
